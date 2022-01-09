5facd855e44bc3c4996bf2a4d0c07ad5bb474464
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 13:44:57 +0200

    Added

---
+++ commit.sh
@@ -0,0 +1,7 @@
+if [`git status | grep "nothing to commit"` != ""]; then
+	read -p "Please, enter a commit description:" m
+	git add *
+	git commit -m $m
+	git push origin
+else echo "Nothing to commit"
+fi
