f0c102898ac913400286d7583fb20702c4f17e38
A Nefedov <alexandr.nefedov.b2w@gmail.com>
2022-01-10 15:00:14 +0200

    Changed commit.sh and stles.css

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@
 status="`git status | grep "nothing to commit"`"
 
 if [ "$status" == "" ]; then
-	read -p "Please, enter a commit description:" m
+	read -p "Please, enter a commit description: " m
 	git add *
 	git commit -m "$m"
 	git push origin
